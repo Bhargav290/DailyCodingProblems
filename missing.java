b5378310d04fce366475f6882526f3fc3db5053b
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-04-28 16:03:02 +0530

    Add files via upload

---
+++ missing.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class missing{
+    public static int firstMissingPositive(int[] nums,
+
+                                           int n)
+    {
+        Arrays.sort(nums);
+
+        int ans = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] == ans)
+                ans++;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args)
+    {
+
+        int []arr = { 3,4,-1,1};
+
+        int n = arr.length;
+
+        int ans = firstMissingPositive(arr, n);
+        System.out.println(ans);
+    }
+}
