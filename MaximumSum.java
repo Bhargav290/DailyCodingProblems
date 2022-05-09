1526702e032779fdbad599601dc5f3bc7a9a2bd1
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-05-09 15:47:04 +0530

    Update MaximumSum.java

--- MaximumSum.java
+++ MaximumSum.java
@@ -1,5 +1,9 @@
+#Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+
+For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.#
 import java.lang.*;
 class MaximumSum {
+       
         int findMaxSum(int []arr, int n)
         {
             int incl = arr[0];
@@ -21,8 +25,7 @@ class MaximumSum {
         public static void main(String[] args)
         {
             MaximumSum sum = new MaximumSum();
-            int []arr = new int[] { 5, 5, 10, 100,
-                    10, 5 };
+            int []arr = new int[] { 2,4,6,2,5};
             int N = arr.length;
 
             System.out.println(
