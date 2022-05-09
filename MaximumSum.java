cc36811977314507cbdb8014101b07e601612dfe
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-05-09 15:44:44 +0530

    Add files via upload

---
+++ MaximumSum.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+class MaximumSum {
+        int findMaxSum(int []arr, int n)
+        {
+            int incl = arr[0];
+            int excl = 0;
+            int excl_new;
+            int i;
+
+            for (i = 1; i < n; i++) {
+                excl_new = Math.max(incl, excl);
+                incl = excl + arr[i];
+                excl = excl_new;
+            }
+
+
+            return Math.max(incl, excl);
+        }
+
+
+        public static void main(String[] args)
+        {
+            MaximumSum sum = new MaximumSum();
+            int []arr = new int[] { 5, 5, 10, 100,
+                    10, 5 };
+            int N = arr.length;
+
+            System.out.println(
+                    sum.findMaxSum(arr, N));
+        }
+    }
+
