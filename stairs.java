31d0db8aad5e6e135edad8d5324d10eaa4062053
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-05-17 14:54:33 +0530

    Update stairs.java

--- stairs.java
+++ stairs.java
@@ -1,3 +1,15 @@
+// question ,
+//There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters//.
+
+For example, if N is 4, then there are 5 unique ways:
+
+1, 1, 1, 1
+2, 1, 1
+1, 2, 1
+1, 1, 2
+2, 2
+
+
 class stairs {
     // A recursive function used by countWays
     static int countWaysUtil(int n, int m) {
