f97f6cf9c61c63706fc6857a62d6def19270190e
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-06-06 18:00:50 +0530

    DCP23.java

--- DCP23.java
+++ DCP23.java
@@ -1,3 +1,34 @@
+// Question:
+// You are given an M by N matrix consisting of booleans that represents a board. 
+// Each True boolean represents a wall. Each False boolean represents a tile you can walk on.
+
+// Given this matrix, a start coordinate, and an end coordinate, return the minimum number of steps required 
+// to reach the end coordinate from the start. If there is no possible path, then return null. 
+// You can move up, left, down, and right. You cannot move through walls. You cannot wrap around the edges 
+// of the board.
+
+// For example, given the following board:
+
+// [[f, f, f, f],
+// [t, t, f, t],
+// [f, f, f, f],
+// [f, f, f, f]]
+
+// and start = (3, 0) (bottom left) and end = (0, 0) (top left), the minimum number of steps required to 
+// reach the end is 7, since we would need to go through (1, 2) because there is a wall everywhere else on 
+// the second row.
+
+
+// Answer:
+
+// We will use Breadth first search approach to solve this question
+// We will use a queue to list all possible paths from start index to end index
+// We will first add all possible paths from start index into the queue, and mark their distance from the start
+
+// Now deque the queue, and check whether it is the end, if yes return distance, if not then enqueue 
+// all the nodes from that node into the queue. Repeat till we find the answer
+
+
 import java.util.ArrayList;
 
     public class DCP23{
