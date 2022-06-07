1604b6c785e4999b11b7d793c2817217b646f377
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-06-07 12:12:59 +0530

    Add files via upload

---
+++ DCP27.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+    public class DCP27
+    {
+        public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+            String line =s.next();
+            System.out.println(balancedParenthesis(line)?"Balanced":"Not Balanced");
+        }
+        public static boolean balancedParenthesis(String line){
+            Stack<Character> stack = new Stack<>();
+            for(int i=0;i<line.length();i++){
+                char ch = line.charAt(i);
+                if(ch == '[' || ch == '(' || ch == '{'){
+                    stack.push(ch);
+                    continue;
+                }
+                if(stack.isEmpty())
+                    return false;
+                char verify = stack.pop();
+                switch(ch){
+                    case ')':if(verify != '(') return false;
+                        break;
+                    case '}':if(verify != '{') return false;
+                        break;
+                    case ']':if(verify != '[') return false;
+                        break;
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+
