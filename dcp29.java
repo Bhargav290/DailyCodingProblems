a2053bbfa247f56da6872c22186fef30b03af9e5
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-06-14 11:32:26 +0530

    Add files via upload

---
+++ dcp29.java
@@ -0,0 +1,24 @@
+import java.util.*;
+    public class dcp29
+    {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String s = sc.next();
+            System.out.println(runLength(s));
+        }
+        public static String runLength(String s){
+            s+=" ";
+            int c = 1;
+            String new_s="";
+            for(int i=0;i<s.length()-1;i++){
+                if(s.charAt(i) == s.charAt(i+1))
+                    c++;
+                else{
+                    new_s+= (""+c)+s.charAt(i);
+                    c=1;
+                }
+            }
+            return new_s;
+
+        }
+    }
