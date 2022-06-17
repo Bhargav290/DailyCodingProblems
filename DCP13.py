6876d4c7eb3f0d072cca3e772ffb5c3c1bae7e65
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-06-17 08:50:08 +0530

    Add files via upload

---
+++ DCP13.py
@@ -0,0 +1,33 @@
+def longest_substring_k_unique(string: str, k: int) -> str:
+    length = len(string)
+    # start and end is used to create the moving window (end - start)
+    start = 0
+    end = 1
+    longest_substring_till_now = string[0]
+    char_freq = {string[0]: 1}
+    num_unique = 1
+    # generating the longest substring
+    while end < length:
+        if string[end] in char_freq:
+            char_freq[string[end]] += 1
+        else:
+            char_freq[string[end]] = 1
+            num_unique += 1
+            # updating moving window and character frequency
+            if num_unique > k:
+                while num_unique > k:
+                    char_freq[string[start]] -= 1
+                    if char_freq[string[start]] == 0:
+                        del char_freq[string[start]]
+                        num_unique -= 1
+                    start += 1
+        # updating the longest substring
+        temp = string[start : end + 1]
+        if num_unique <= k and len(temp) > len(longest_substring_till_now):
+            longest_substring_till_now = temp
+        end += 1
+    return longest_substring_till_now
+
+
+if __name__ == "__main__":
+    print(longest_substring_k_unique("abcba", 2))
