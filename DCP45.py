992b3cd54cdd4e425fd800962360b53baee4b6c1
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-07-03 16:16:13 +0530

    Update DCP45.py

--- DCP45.py
+++ DCP45.py
@@ -1,3 +1,9 @@
+"""
+Problem:
+Using a function rand5() that returns an integer from 1 to 5 (inclusive) with uniform
+probability, implement a function rand7() that returns an integer from 1 to 7
+(inclusive).
+"""
 from random import randint
 import matplotlib.pyplot as plt
 
