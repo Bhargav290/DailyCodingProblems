3b91ed9907739e117ac3937d6015f62b687f4300
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-07-03 16:10:13 +0530

    Add files via upload

---
+++ MaxStack.py
@@ -0,0 +1,63 @@
+from DataStructures.Stack import Stack
+
+
+class MaxStack:
+    def __init__(self) -> None:
+        self.stack = Stack()
+        self.maximum = Stack()
+
+    def __repr__(self) -> str:
+        return f"Stack: {self.stack}\nMax Stack: {self.maximum}"
+
+    def push(self, val: int) -> None:
+        self.stack.push(val)
+        # if the current value is larger than the previous maxima, its index is added
+        # to the maximum stack
+        if self.maximum.is_empty() or self.stack[self.maximum.peek()] < val:
+            self.maximum.push(len(self.stack) - 1)
+
+    def pop(self) -> int:
+        if self.stack.is_empty():
+            raise RuntimeError("Cannot pop from a empty stack")
+        # if the index of the current element to be removed is in the maximum stack,
+        # its removed as well
+        if len(self.stack) == self.maximum.peek() + 1:
+            self.maximum.pop()
+        return self.stack.pop()
+
+    def max(self) -> int:
+        if self.stack.is_empty():
+            raise RuntimeError("Cannot get max of a empty stack")
+        # the maximum is accessed from the last poistion stored in maximum stack
+        return self.stack[self.maximum.peek()]
+
+
+if __name__ == "__main__":
+    s = MaxStack()
+
+    s.push(1)
+    s.push(3)
+    s.push(2)
+    s.push(5)
+
+    print(s.max())
+    print(s)
+    print(s.pop())
+    print()
+
+    print(s.max())
+    print(s)
+    print(s.pop())
+    print()
+
+    print(s.max())
+    print(s)
+    print(s.pop())
+    print()
+
+    print(s.max())
+    print(s)
+    print(s.pop())
+    print()
+
+    print(s)
