aab5f31ea708d07925eb160c6ab0cb7be7fd4590
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-07-03 16:26:40 +0530

    Update DCP48.py

--- DCP48.py
+++ DCP48.py
@@ -1,3 +1,17 @@
+"""
+Problem:
+Given pre-order and in-order traversals of a binary tree, write a function to reconstruct the tree.
+For example, given the following preorder traversal:
+[a, b, d, e, c, f, g]
+And the following inorder traversal:
+[d, b, e, a, f, c, g]
+You should return the following tree:
+    a
+   / \
+  b   c
+ / \ / \
+d  e f  g
+"""
 from typing import List
 
 from DataStructures.Tree import BinaryTree, Node
