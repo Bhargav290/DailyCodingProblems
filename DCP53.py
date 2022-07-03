a434c9bfc6f08d0ccefb016b6691ec08351524df
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-07-03 16:35:27 +0530

    Update DCP53.py

--- DCP53.py
+++ DCP53.py
@@ -1,3 +1,9 @@
+"""
+Problem:
+Implement a queue using two stacks. Recall that a queue is a FIFO (first-in, first-out)
+data structure with the following methods: enqueue, which inserts an element into the
+queue, and dequeue, which removes it.
+"""
 from DataStructures.Stack import Stack
 
 
