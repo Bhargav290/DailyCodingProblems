066809d6e35cbbd84afbb4018ae26ea9b7a405b5
Bhargav290 <92253011+Bhargav290@users.noreply.github.com>
2022-07-03 16:18:59 +0530

    Add files via upload

---
+++ DCP46.py
@@ -0,0 +1,17 @@
+def is_palindrome(string: str) -> bool:
+    # helper function to check if a string is a palindrome
+    return string == string[::-1]
+
+
+def get_longest_palindrome_substring(string: str) -> str:
+    if is_palindrome(string):
+        return string
+    # generating the longest palindromic substring
+    string1 = get_longest_palindrome_substring(string[1:])
+    string2 = get_longest_palindrome_substring(string[:-1])
+    return max(string1, string2, key=lambda s: len(s))
+
+
+if __name__ == "__main__":
+    print(get_longest_palindrome_substring("aabcdcb"))
+    print(get_longest_palindrome_substring("bananas"))
